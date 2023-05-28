649765ddac1d3945a077a0af1cb2ca8c10822c18
sslivkoff <stormslivkoff@gmail.com>
2023-05-28 09:33:38 -0700

    flesh out ls cli subcommand

--- rpc_bench/cli/ls_command.py
+++ rpc_bench/cli/ls_command.py
@@ -13,7 +13,27 @@ def get_command_spec() -> toolcli.CommandSpec:
 
 
 def ls_command() -> None:
-    print('Available tests:')
+    import toolstr
+
+    toolstr.print_text_box(
+        'Available tests',
+        text_style=rpc_bench.styles['metavar'],
+        style=rpc_bench.styles['content'],
+    )
+
+    print()
+    toolstr.print_header(
+        'Single Load Tests',
+        style=rpc_bench.styles['content'],
+        text_style=rpc_bench.styles['metavar'],
+    )
     for test in rpc_bench.get_single_test_generators():
-        print('-', test)
+        toolstr.print_bullet(key=test, value='', colon_str='')
+    print()
+    toolstr.print_header(
+        'Multi Load Tests',
+        style=rpc_bench.styles['content'],
+        text_style=rpc_bench.styles['metavar'],
+    )
+    toolstr.print_bullet(key='all', value='', colon_str='')
 
