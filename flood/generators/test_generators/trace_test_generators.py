8e4b3d0e2b84c73c9833448d2edf15fb12efb60e
sslivkoff <stormslivkoff@gmail.com>
2023-06-02 16:29:24 -0700

    trace tests

---
+++ flood/generators/test_generators/trace_test_generators.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import typing
+
+import flood
+from flood import spec
+
+
+def generate_test_trace_block(
+    *,
+    network: str,
+    rates: typing.Sequence[int],
+    duration: int | None = None,
+    durations: typing.Sequence[int] | None = None,
+    vegeta_kwargs: typing.Mapping[str, str | None] | None = None,
+    random_seed: spec.RandomSeed | None = None,
+) -> spec.LoadTest:
+    n_calls = flood.estimate_call_count(
+        rates=rates, duration=duration, durations=durations
+    )
+    calls = flood.generate_calls_trace_block(n_calls=n_calls, network=network)
+    return flood.construct_load_test(
+        calls=calls,
+        rates=rates,
+        duration=duration,
+        durations=durations,
+    )
+
+
+def generate_test_trace_transaction(
+    *,
+    network: str,
+    rates: typing.Sequence[int],
+    duration: int | None = None,
+    durations: typing.Sequence[int] | None = None,
+    vegeta_kwargs: typing.Mapping[str, str | None] | None = None,
+    random_seed: spec.RandomSeed | None = None,
+) -> spec.LoadTest:
+    n_calls = flood.estimate_call_count(
+        rates=rates, duration=duration, durations=durations
+    )
+    calls = flood.generate_calls_trace_transaction(
+        n_calls=n_calls, network=network
+    )
+    return flood.construct_load_test(
+        calls=calls,
+        rates=rates,
+        duration=duration,
+        durations=durations,
+    )
+
+
+def generate_test_trace_replay_block_transactions(
+    *,
+    network: str,
+    rates: typing.Sequence[int],
+    duration: int | None = None,
+    durations: typing.Sequence[int] | None = None,
+    vegeta_kwargs: typing.Mapping[str, str | None] | None = None,
+    random_seed: spec.RandomSeed | None = None,
+) -> spec.LoadTest:
+    n_calls = flood.estimate_call_count(
+        rates=rates, duration=duration, durations=durations
+    )
+    calls = flood.generate_calls_trace_replay_block_transactions(
+        n_calls=n_calls, network=network
+    )
+    return flood.construct_load_test(
+        calls=calls,
+        rates=rates,
+        duration=duration,
+        durations=durations,
+    )
+
+
+def generate_test_trace_replay_transaction(
+    *,
+    network: str,
+    rates: typing.Sequence[int],
+    duration: int | None = None,
+    durations: typing.Sequence[int] | None = None,
+    vegeta_kwargs: typing.Mapping[str, str | None] | None = None,
+    random_seed: spec.RandomSeed | None = None,
+) -> spec.LoadTest:
+    n_calls = flood.estimate_call_count(
+        rates=rates, duration=duration, durations=durations
+    )
+    calls = flood.generate_calls_trace_replay_transaction(
+        n_calls=n_calls, network=network
+    )
+    return flood.construct_load_test(
+        calls=calls,
+        rates=rates,
+        duration=duration,
+        durations=durations,
+    )
+
