d295faa0fa04f411d7a6356aa4f9c5022d93553e
sslivkoff <stormslivkoff@gmail.com>
2023-06-06 14:04:49 -0700

    skip node tests if env vars unset

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,20 +12,35 @@ def get_env_value(name):
 
 @pytest.fixture
 def flood_test_local_node_1():
+    skip_if_env_nodes_unset()
     return get_env_value('FLOOD_TEST_LOCAL_NODE_1')
 
 
 @pytest.fixture
 def flood_test_local_node_2():
+    skip_if_env_nodes_unset()
     return get_env_value('FLOOD_TEST_LOCAL_NODE_2')
 
 
 @pytest.fixture
 def flood_test_remote_node_1():
+    skip_if_env_nodes_unset()
     return get_env_value('FLOOD_TEST_REMOTE_NODE_1')
 
 
 @pytest.fixture
 def flood_test_remote_node_2():
+    skip_if_env_nodes_unset()
     return get_env_value('FLOOD_TEST_REMOTE_NODE_2')
+
+
+def skip_if_env_nodes_unset():
+    for var in [
+        'FLOOD_TEST_LOCAL_NODE_1',
+        'FLOOD_TEST_LOCAL_NODE_2',
+        'FLOOD_TEST_REMOTE_NODE_1',
+        'FLOOD_TEST_REMOTE_NODE_2',
+    ]:
+        if os.getenv(var) is None:
+            pytest.skip(reason=var + ' env var not set')
 
