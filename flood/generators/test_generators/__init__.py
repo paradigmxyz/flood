643ab9c92e7bbc0f397be70ff299d9a7efe27721
sslivkoff <stormslivkoff@gmail.com>
2023-06-19 21:51:39 -0700

    add stub for multi test generators

--- flood/generators/test_generators/__init__.py
+++ flood/generators/test_generators/__init__.py
@@ -3,5 +3,6 @@ from .block_test_generators import *
 from .contract_test_generators import *
 from .generic_test_generators import *
 from .log_test_generators import *
+from .multi_test_generators import *
 from .transaction_test_generators import *
 from .trace_test_generators import *
---
+++ flood/generators/test_generators/multi_test_generators.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import typing
+
+import flood
+
+
+def generate_tests_all_methods() -> typing.Mapping[str, flood.LoadTest]:
+    raise Exception()
+
