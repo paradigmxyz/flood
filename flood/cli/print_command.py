923f8838e62ce1ac15dabe5267a331e2510c0f2c
sslivkoff <stormslivkoff@gmail.com>
2023-07-17 15:34:46 -0700

    add aggregation mode to print cli command

--- flood/cli/print_command.py
+++ flood/cli/print_command.py
@@ -12,7 +12,11 @@ def get_command_spec() -> toolcli.CommandSpec:
         'f': print_command,
         'help': 'print output of previous test results',
         'args': [
-            {'name': 'output_dir', 'help': 'output directory of previous test'},
+            {
+                'name': 'output_dirs',
+                'help': 'output directory of previous test(s)',
+                'nargs': '+',
+            },
             {
                 'name': ('-m', '--metrics'),
                 'help': 'space-separated list of performance metrics',
@@ -22,8 +26,20 @@ def get_command_spec() -> toolcli.CommandSpec:
     }
 
 
-def print_command(output_dir: str, metrics: typing.Sequence[str]) -> None:
+def print_command(
+    output_dirs: typing.Sequence[str], metrics: typing.Sequence[str]
+) -> None:
+    if len(output_dirs) == 1:
+        print_single(output_dirs[0], metrics)
+    elif len(output_dirs) > 1:
+        print_multiple(output_dirs)
+    else:
+        raise Exception('must specify at least one output_dir')
 
+
+def print_single(
+    output_dir: str, metrics: typing.Sequence[str]
+) -> None:
     test_payload = flood.load_single_run_test_payload(output_dir)
     test = flood.generate_test(**test_payload['test_parameters'])
     results_payload = flood.load_single_run_results_payload(output_dir)
@@ -56,4 +72,38 @@ def print_command(output_dir: str, metrics: typing.Sequence[str]) -> None:
         metrics=metrics,
         indent=4,
     )
+
+
+def print_multiple(output_dirs: typing.Sequence[str]) -> None:
+    # load results
+    test_payloads = []
+    outputs = []
+    for output_dir in output_dirs:
+        test_payload = flood.load_single_run_test_payload(output_dir)
+        output = flood.load_single_run_results_payload(output_dir)
+        test_payloads.append(test_payload)
+        outputs.append(output)
+
+    # print max throughput
+    rows = []
+    conditions = list(outputs[0]['results'].keys())
+    for test_payload, output in zip(test_payloads, outputs):
+        test_parameters = test_payload['test_parameters']
+        row: list[typing.Any] = [test_parameters['test_name']]
+        for condition in conditions:
+            results = output['results'][condition]
+            throughputs = [tp for tp in results['throughput'] if tp is not None]
+            if len(throughputs) > 0:
+                row.append(max(throughputs))
+            else:
+                row.append(None)
+        row.append(max(results['target_rate']))
+        rows.append(row)
+    labels = ['test'] + conditions + ['max tested']
+
+    flood.print_text_box('Max Throughput')
+    print()
+    print('units = requests per second')
+    print()
+    flood.print_table(rows, labels=labels)
 
